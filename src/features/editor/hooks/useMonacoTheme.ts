e87efad9dfbd0818163754d949346c2bf9a5ae97
Jay <netneuron1@gmail.com>
2024-12-27 15:51:51 -0500

    Update useMonacoTheme.ts

--- src/features/editor/hooks/useMonacoTheme.ts
+++ src/features/editor/hooks/useMonacoTheme.ts
@@ -1,50 +1,29 @@
-import * as tf from '@tensorflow/tfjs';
-import { ModelArchitecture, LayerConfig } from './types';
+import { useEffect } from 'react';
+import { loader } from '@monaco-editor/react';
+import { editor } from 'monaco-editor';
 
-export async function createModel(architecture: ModelArchitecture): Promise<tf.LayersModel> {
-  const model = tf.sequential();
-  
-  architecture.layers.forEach((layer, index) => {
-    const config: tf.layers.LayerArgs = {
-      units: layer.units,
-      activation: layer.activation || 'relu',
-      ...layer.config
-    };
+const matrixTheme: editor.IStandaloneThemeData = {
+  base: 'vs-dark' as const,
+  inherit: true,
+  rules: [
+    { token: 'comment', foreground: '4C9855' },
+    { token: 'string', foreground: '24FF00' }
+  ],
+  colors: {
+    'editor.background': '#0D0208',
+    'editor.foreground': '#00FF41',
+    'editor.lineHighlightBackground': '#00FF4120',
+    'editor.selectionBackground': '#00FF4140',
+    'editorCursor.foreground': '#00FF41',
+    'editorLineNumber.foreground': '#00FF4180'
+  }
+};
 
-    if (index === 0) {
-      if (layer.type === 'lstm') {
-        config.inputShape = [1, ...architecture.inputShape];
-        config.returnSequences = true;
-      } else {
-        config.inputShape = architecture.inputShape;
-      }
-    }
-
-    switch (layer.type) {
-      case 'dense':
-        model.add(tf.layers.dense(config));
-        break;
-      case 'lstm':
-        model.add(tf.layers.lstm(config));
-        break;
-      case 'attention':
-        model.add(createAttentionLayer(config));
-        break;
-    }
-  });
-
-  model.compile({
-    optimizer: tf.train.adam(0.001),
-    loss: 'meanSquaredError',
-    metrics: ['accuracy']
-  });
-
-  return model;
-}
-
-function createAttentionLayer(config: tf.layers.LayerArgs): tf.layers.Layer {
-  return tf.layers.dense({
-    ...config,
-    activation: 'softmax'
-  });
+export function useMonacoTheme() {
+  useEffect(() => {
+    loader.init().then(monaco => {
+      monaco.editor.defineTheme('matrix', matrixTheme);
+      monaco.editor.setTheme('matrix');
+    });
+  }, []);
 }
